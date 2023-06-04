af7ba3304a350610de3de9af4cea7b3a03c0b748
karianmash <ianmachariak17@gmail.com>
2023-06-04 13:18:52 +0300

    Add login with google and facebook buttons

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,12 +5,9 @@
 <x-app-layout :title="$title">
 
     <main>
-
         <div class="loginArea login_section">
             <div class="container">
-
                 <div class="login-Wrapper">
-
                     <div class="heading_title">
                         <h1>Create An Account</h1>
                         <p>To get started, please create an account by filling out the form below. Once you've
@@ -22,7 +19,7 @@
                         @csrf
 
                         <div class="col-md-12">
-                            <label class="infoTitle" id="email">Email* </label>
+                            <label class="infoTitle" id="email">Email*</label>
                             <x-basic-input required autofocus autocomplete="email" :name="__('email')" :type="__('email')"
                                 :placeholder="__('Your email address')" />
                         </div>
@@ -51,24 +48,30 @@
                         </div>
 
                         <div class="col-sm-12">
-
                             <p class="terms_register mb-10">By continuing, you agree to our <a href=""
-                                    class="link_underline">Terms and
-                                    conditions</a></p>
-
+                                    class="link_underline">Terms and conditions</a></p>
                             <div class="btn-wrapper text-center">
                                 <button type="submit" class="cmn-btn4 w-100 mb-30">Register</button>
-                                <p class="sinUp"><span>Already have an account?</span><a href="{{ route('login') }}"
-                                        class="singApp">Login Here</a></p>
                             </div>
                         </div>
                     </form>
+
+                    <div class="row">
+                        <p class="mb-20 text-center">Or register with:</p>
+                        <div class="social-login-buttons" style="display: flex; justify-content: space-between">
+                            <a href="#" class="btn"
+                                style="background-color: #DB4437; color: #fff; width: 47%">Google</a>
+                            <a href="#" class="btn"
+                                style="background-color: #3B5998; color: #fff; width: 47%">Facebook</a>
+                        </div>
+                        <p class="sinUp text-center mt-4">
+                            <span>Already have an account?</span><a href="{{ route('login') }}" class="singApp">Login
+                                Here</a>
+                        </p>
+                    </div>
                 </div>
-
             </div>
-
         </div>
-
     </main>
 
 </x-app-layout>
