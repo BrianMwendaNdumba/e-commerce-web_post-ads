b267076f4c32b53241ea5f3204c214c76ed5a787
karianmash <ianmachariak17@gmail.com>
2023-06-04 13:17:30 +0300

    Register the event for sending welcome email

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\SendWelcomeMessage;
+use App\Listeners\SendWelcomeMessageListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,6 +19,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        SendWelcomeMessage::class => [
+            SendWelcomeMessageListener::class,
         ],
     ];
 
