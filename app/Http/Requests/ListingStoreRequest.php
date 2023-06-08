6d2d90e6b245099726c238b835ead54cadc19563
karianmash <ianmachariak17@gmail.com>
2023-06-08 10:05:37 +0300

    Import and apply image validation rules from a separate file

--- app/Http/Requests/ListingStoreRequest.php
+++ app/Http/Requests/ListingStoreRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\ImageFilterRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ListingStoreRequest extends FormRequest
@@ -21,24 +22,36 @@ class ListingStoreRequest extends FormRequest
      */
     public function rules(): array
     {
+        $category = $this->input('category_id');
+
         return [
             'category_id' => ['required', 'string', 'max:2'],
             'title' => ['required', 'string', 'max:80'],
-            'condition' => ['nullable', 'string', 'max:10'],
+            'condition' => ['nullable', 'string', 'max:30'],
+            'mileage' => ['nullable', 'string', 'max:255'],
+            'engine' => ['nullable', 'string', 'max:30'],
             'price' => ['required', 'string', 'max:255'],
             'location' => ['required', 'string', 'max:255'],
-            'description' => ['required', 'string', 'max:1024'],
+            'description' => [
+                'required',
+                'string',
+                function ($attribute, $value, $fail) use ($category) {
+                    $maxCharacters = ($category == 1) ? 2000 : (($category == 5) ? 3500 : 1024);
+                    if (strlen($value) > $maxCharacters) {
+                        $fail("The $attribute may not exceed $maxCharacters characters for the selected category.");
+                    }
+                },
+            ],
             'is_negotiable' => ['nullable', 'string', 'max:1'],
-            'images' => ['required', 'array', 'max:10'],
-            'images.*' => 'nullable|mimes:jpg,jpeg,png|max:2048',
+            'images' => ['required', 'array', 'max:5'],
         ];
     }
 
     public function messages()
     {
         return [
-            'images.max' => 'Max number of images is 10',
-            'images.*.mimes' => 'Profile Photo must be .jpg, .jpeg or .png',
+            'images.max' => 'Max number of images is 5',
+            'images.*.mimes' => 'Profile Photo must be .jpg, or .jpeg',
             'images.*.max' => 'Profile Photo must 2MB or less',
             'images' => 'Please upload images for your ad',
             'category_id' => 'Please select a category'
