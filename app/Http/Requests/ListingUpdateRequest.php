d7e3ef10af4edd33a295a1916c25459f7f7b8c21
karianmash <ianmachariak17@gmail.com>
2023-06-08 10:06:56 +0300

    Upload validation rules for various form fields

--- app/Http/Requests/ListingUpdateRequest.php
+++ app/Http/Requests/ListingUpdateRequest.php
@@ -21,16 +21,30 @@ class ListingUpdateRequest extends FormRequest
      */
     public function rules(): array
     {
+        $category = $this->input('category_id');
+
+        $descriptionMax = ($category == 1) ? 2000 : (($category == 5) ? 3500 : 1024);
+
         return [
             'category_id' => ['required', 'string', 'max:2'],
             'title' => ['required', 'string', 'max:80'],
-            'condition' => ['nullable', 'string', 'max:10'],
+            'condition' => ['nullable', 'string', 'max:30'],
+            'mileage' => ['nullable', 'string', 'max:255'],
+            'engine' => ['nullable', 'string', 'max:30'],
             'price' => ['required', 'string', 'max:255'],
-            'description' => ['required', 'string', 'max:1024'],
+            'location' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string', 'max:' . $descriptionMax],
             'is_negotiable' => ['nullable', 'string', 'max:1'],
-            'images' => ['nullable', 'array', 'max:10'],
+            'images' => ['nullable', 'array', 'max:5'],
             'preloaded' => ['nullable'],
-            'images.*' => 'nullable|mimes:jpg,jpeg,png|max:2048',
+            'images.*' => 'nullable|mimes:jpg,jpeg|max:2048',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'description.max' => 'The :attribute may not exceed :max characters for the selected category.',
         ];
     }
 }
